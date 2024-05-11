697376fb6e3ce611a46a0cbffd6a5f74ea038109
Simon Mei <smei8800@gmail.com>
2024-05-11 19:13:06 -0400

    Add color param to List

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import "./style.css";
 
 const listContainer = document.querySelector(".list-container");
 const taskContainer = document.querySelector(".task-container");
+
+// Modal logic
 const listModalButton = document.querySelector(".list-modal-button");
 const listModal = document.querySelector("#list-modal");
+const listModalCancelButton = document.querySelector("#list-cancel-btn");
+const listModalOkayButton = document.querySelector("#list-ok-btn");
 
 listModalButton.onclick = function () {
   listModal.style.display = "block";
+};
+
+listModalCancelButton.onclick = function () {
+  listModal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == listModal) {
+    listModal.style.display = "none";
+  }
+};
+
+listModalOkayButton.onclick = function (event) {
+  event.preventDefault();
+  let listName = document.getElementById("list-name");
+  let listColor = document.getElementById("list-color");
+  let newList = new List({ name: listName, color: listColor });
 };
--- src/modules/list.js
+++ src/modules/list.js
@@ -2,8 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 
 export default class List {
   constructor(options = {}) {
-    const { name = "List name" } = options;
+    const { name = "List name", color = "red" } = options;
     this.id = uuidv4();
+    this.color = color;
     this.name = name;
     this.todos = [];
   }
